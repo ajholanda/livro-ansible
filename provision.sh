8daf3d58b706452fbd4c8390068f1267d154448a
Adriano J. Holanda <ajholanda@gmail.com>
2023-02-24 12:03:08 -0300

    improve comments and echo output

--- provision.sh
+++ provision.sh
@@ -1,14 +1,31 @@
 VAGRANT_HOME="/home/vagrant"
-mkdir -p $VAGRANT_HOME/.ssh
-# cp /vagrant/provision/id_ed25519* $VAGRANT_HOME/.ssh
-# cp /vagrant/provision/id_ed25519.pub $VAGRANT_HOME/.ssh/authorized_keys
-# chmod 400 $VAGRANT_HOME/.ssh/id_ed25519*
-cp /vagrant/provision/ssh_config $VAGRANT_HOME/.ssh/config
-chmod 400 $VAGRANT_HOME/.ssh/config
-chown -R vagrant:vagrant $VAGRANT_HOME/.ssh/
+
+# PACKAGE
 # Update package sources list
-echo $(hostname)
-which apt-get && echo 'APT: Update the list of packages.'; apt-get update >/dev/null
+which apt-get >/dev/null && \
+    echo 'APT: Updating the list of packages...'; apt-get update >/dev/null
+# Install ansible on the controller
+if [ "$(hostname)" == "ansible" ]; then
+    echo "APT: Installing ansible..."
+    apt-get install -y ansible >/dev/null
+fi
+
+# SSH
+# Create ssh directory for vagrant user
+mkdir -p $VAGRANT_HOME/.ssh
+# Copy ssh keys
+cp /vagrant/provision/id_ed25519* $VAGRANT_HOME/.ssh
+# Fix the permissions of the keys files
+chmod 400 $VAGRANT_HOME/.ssh/id_ed25519*
+# Write the public key to authorized keys list
+cp /vagrant/provision/id_ed25519.pub $VAGRANT_HOME/.ssh/authorized_keys
+# Copy the ssh client configuration for user vagrant
+cp /vagrant/provision/ssh_config $VAGRANT_HOME/.ssh/config
+# Fix the permission of the ssh client configuration
+chmod 400 $VAGRANT_HOME/.ssh/config
+# Fix the owner of ssh directory for vagrant user
+chown -R vagrant:vagrant $VAGRANT_HOME/.ssh/
+
 # Allow password authentication via SSH
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
