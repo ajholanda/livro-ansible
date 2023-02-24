3982f26bd21749a4ef0c10bb8083fabb3cbacec1
Adriano J. Holanda <ajholanda@gmail.com>
2023-02-24 11:27:05 -0300

    comment copy of keys

--- provision.sh
+++ provision.sh
@@ -1,13 +1,14 @@
 VAGRANT_HOME="/home/vagrant"
 mkdir -p $VAGRANT_HOME/.ssh
-cp /vagrant/provision/id_ed25519* $VAGRANT_HOME/.ssh
-cp /vagrant/provision/id_ed25519.pub $VAGRANT_HOME/.ssh/authorized_keys
-chmod 400 $VAGRANT_HOME/.ssh/id_ed25519*
+# cp /vagrant/provision/id_ed25519* $VAGRANT_HOME/.ssh
+# cp /vagrant/provision/id_ed25519.pub $VAGRANT_HOME/.ssh/authorized_keys
+# chmod 400 $VAGRANT_HOME/.ssh/id_ed25519*
 cp /vagrant/provision/ssh_config $VAGRANT_HOME/.ssh/config
 chmod 400 $VAGRANT_HOME/.ssh/config
 chown -R vagrant:vagrant $VAGRANT_HOME/.ssh/
 # Update package sources list
-which apt && apt update
+echo $(hostname)
+which apt-get && echo 'APT: Update the list of packages.'; apt-get update >/dev/null
 # Allow password authentication via SSH
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
