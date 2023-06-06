d5593bebd553080d5de8bb11341c44077b90c215
Adriano J. Holanda <ajholanda@gmail.com>
2023-06-06 17:09:45 -0300

    add --debug option

--- ansible-cmds.sh
+++ ansible-cmds.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+DEBUG=0
+
 function ECHO {
 	echo >&2 "INFO [$1]"
 }
 
 function RUN {
 	echo $1
+	if [ $DEBUG -eq 1 ]; then
+		read -p 'Press any key to continue: ' tmp
+	fi
 	eval $1 && test 1 -eq 1
 	sleep 3
 }
@@ -100,25 +105,29 @@ function exec_playbook_cmds() {
 
 	ECHO "Executa tarefas que não tenham etiquetas"
 	RUN 'ansible-playbook tags.yml --tags untagged'
+}
 
-	# When
+function exec_vars_cmds() {
+	# Special variables and When
 	ECHO "Executa o playbook webserver-Debian.yml"
 	RUN 'ansible-playbook webserver-Debian.yml'
 
 	ECHO "Executa o playbook webserver-distro.yml"
 	RUN 'ansible-playbook webserver-distro.yml'
-}
 
-function exec_vars_cmds() {
+	# Facts
 	ECHO "Lista as variáveis mágicas do sistema no host web.exemplo"
 	RUN 'ansible -m setup web.example.net'
 
+	# Playbook variables
 	ECHO "Executa o playbook webserver-vars.yml"
 	RUN 'ansible-playbook webserver-vars.yml'
 
+	# Host variables
 	ECHO "Executa o playbook webserver-host_vars.yml"
 	RUN 'ansible-playbook webserver-host_vars.yml'
 
+	# Register
 	ECHO "Usa register para armazenar o status de um arquivo"
 	RUN 'ansible-playbook register.yml'
 }
@@ -141,24 +150,8 @@ function exec_crypto_cmds() {
 	RUN "ansible-vault encrypt --vault-id prod@vault-prod.txt sssd/templates/sssd.conf.j2 --encrypt-vault-id prod 2>/dev/null"
 }
 
-function exec_windows_cmds() {
-	galaxy
-
-	# win_chocolatey module
-	ECHO "Instala o LibreOffice no sistem Windows"
-	RUN "ansible-playbook labs.yml --tags libreoffice --limit windows"
-
-	# win_powershell module
-	ECHO "Executa um script PowerShell para agendar desligamento"
-	RUN "ansible-playbook labs.yml --tags power --limit windows"
-
-	# win_user module
-	ECHO "Gerencia usuários no Windows"
-	RUN "ansible-playbook  labs.yml --tags user --limit windows"
-}
-
-# Recursos adicionais
-function exec_resources() {
+# Ansible Galaxy
+function exec_galaxy() {
 	ECHO "Faz download do role ajholanda.x2goclient"
 	RUN "ansible-galaxy role install --roles-path ./roles ajholanda.x2goclient"
 
@@ -171,6 +164,22 @@ function exec_resources() {
 	ECHO "Instala os módulos para o Windows"
 	RUN "ansible-galaxy collection install ansible.windows"
 	RUN "ansible-galaxy collection install chocolatey.chocolatey"
+}
+
+function exec_windows_cmds() {
+	galaxy
+
+	# win_chocolatey module
+	ECHO "Instala o LibreOffice no sistem Windows"
+	RUN "ansible-playbook desktops.yml --tags libreoffice --limit windows"
+
+	# win_powershell module
+	ECHO "Executa um script PowerShell para agendar desligamento"
+	RUN "ansible-playbook desktops.yml --tags power --limit windows"
+
+	# win_user module
+	ECHO "Gerencia usuários no Windows"
+	RUN "ansible-playbook  desktops.yml --tags user --limit windows"
 }
 
 # Casos de uso
@@ -188,9 +197,9 @@ function exec_usecases_cmds() {
 	ECHO "Instala o VS Code no host alice"
 	RUN "ansible-playbook desktops.yml --limit inf1.example.net --tags vscode"
 	ECHO "Remove o Google Chrome do host pedro"
-	RUN "ansible-playbook desktops.yml --limit off1.example.net --tags googlechrome --extra-vars 'package_install=absent'"
+	RUN "ansible-playbook desktops.yml --limit off1.example.net --tags googlechrome --extra-vars 'package_state=absent'"
 	ECHO "Atualiza todos os programas do grupo desktops"
-	RUN "ansible-playbook desktops.yml --extra-vars 'package_install=latest'"
+	RUN "ansible-playbook desktops.yml --extra-vars 'package_state=latest'"
 
 	ECHO "Força uma atualização no Windows usando o Windows Update (com reboots)"
 	RUN "ansible-playbook desktops.yml --tags win_updates"
@@ -210,7 +219,7 @@ function exec_linux_cmds() {
 
 USAGE=$(
 	cat <<-EOM
-		$0 [--all | -c[2..10] | --galaxy | --linux | --windows]
+		$0 [--all | -c[2..10] | --galaxy | --linux | --windows] [--debug]
 		onde cada opção executa os comandos:
 		  --all     todos
 		  -c2       do Capítulo 2 - comandos ad-hoc
@@ -219,14 +228,19 @@ USAGE=$(
 		  -c5       do Capítulo 5 - roles
 		  -c6       do Capítulo 6 - gabarito
 		  -c7       do Capítulo 7 - criptografia
-		  -c8       do Capítulo 8 - Windows
-		  -c9       do Capítulo 9 - recursos adicionais
+		  -c8       do Capítulo 8 - Ansible Galaxy
+		  -c9       do Capítulo 9 - Windows
 		  -c10      do Capítulo 10 - casos de uso
 		  --galaxy  de atualização dos roles do Ansible do Ansible Galaxy
 		  --linux   para os sistemas Linux
 		  --windows para os sistemas Windows
+		--debug: pausa a execução a cada comando.
 	EOM
 )
+
+if [ $2 == "--debug" ]; then
+	DEBUG=1
+fi
 
 case $1 in
 "--all")
@@ -251,10 +265,10 @@ case $1 in
 	exec_crypto_cmds
 	;;
 "-c8")
-	exec_windows_cmds
+	exec_galaxy
 	;;
 "-c9")
-	exec_resources
+	exec_windows_cmds
 	;;
 "-c10")
 	exec_usecases_cmds
