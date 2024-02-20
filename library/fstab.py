171c92fb6c270c6025d9a71f512ae60cb34c6164
Adriano J. Holanda <ajholanda@gmail.com>
2024-02-20 13:18:10 -0300

    remove shebang and __metaclass__ declaration

--- library/fstab.py
+++ library/fstab.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python
-
 # Copyright: (c) 2024, Adriano J. Holanda <ajholanda@example.net>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-__metaclass__ = type
+
 
 DOCUMENTATION = r'''
 ---
