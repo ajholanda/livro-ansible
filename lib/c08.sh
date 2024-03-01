5cac4c1044a09ab47a3a49617e2f45d46e81107c
Adriano J. Holanda <ajholanda@gmail.com>
2024-03-01 17:33:10 -0300

    add cmd to test fstab module

--- lib/c08.sh
+++ lib/c08.sh
@@ -18,6 +18,8 @@ function exec_dev_cmds() {
 	## Filtro
 	ECHO "Executa e exemplo que mostra o uso do plugin filtro"
 	RUN "ansible-playbook plugin.yml"
+
+	## Módulos
+	ECHO "Testa o módulo fstab."
+	RUN "ansible-playbook test_module.yml"
 }
-
-
