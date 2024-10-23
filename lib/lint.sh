4e15f0e08e42dd3148deda283a9fba2b6934986c
Adriano J. Holanda <ajholanda@gmail.com>
2024-10-23 12:23:18 -0300

    1st commit

---
+++ lib/lint.sh
@@ -0,0 +1,13 @@
+ECHO "Verifica a sintaxe do arquivo lint_v0.yml."
+RUN "ansible-lint lint_v0.yml"
+
+
+ECHO "Verifica a sintaxe do arquivo lint_v1.yml."
+RUN "ansible-lint lint_v1.yml"
+
+ECHO "Verifica a sintaxe do arquivo lint_v1.yml com o comportamento alterado por lint-config.yml."
+RUN "ansible-lint -c lint-config.yml lint_v1.yml"
+
+ECHO "Verifica a sintaxe do arquivo lint_v2.yml."
+RUN "ansible-lint lint_v2.yml"
+
