841ea59e761803b14c264686d368b7195bbeadcd
Adriano J. Holanda <ajholanda@gmail.com>
2024-10-24 11:08:03 -0300

    add command to list ansible-lint rules

--- lib/lint.sh
+++ lib/lint.sh
@@ -11,3 +11,5 @@ RUN "ansible-lint -c lint-config.yml lint_v1.yml"
 ECHO "Verifica a sintaxe do arquivo lint_v2.yml."
 RUN "ansible-lint lint_v2.yml"
 
+ECHO "Lista todas as regras do ansible-lint."
+RUN "ansible-lint --list-rules --verbose"
