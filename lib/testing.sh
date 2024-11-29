4040977173f64eaa96bb0a02c6e43ac8038cd4ff
Adriano J. Holanda <ajholanda@gmail.com>
2024-11-29 12:28:14 -0300

    add command to execute assert for blender test

--- lib/testing.sh
+++ lib/testing.sh
@@ -1,2 +1,6 @@
 ECHO "Asserção para assegurar o uso de criptografia nas senhas."
 RUN "ansible-playbook assert.yml --tags crypto"
+
+ECHO "Asserção para testar se a quantidade de memória é suficiente para instalar o Blender."
+RUN "ansible-playbook assert.yml --tags blender"
+
