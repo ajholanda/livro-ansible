fdc91c6e05b28f3cacf8346f80f0152864c63504
Adriano J. Holanda <ajholanda@gmail.com>
2024-12-04 11:48:45 -0300

    add command to run multiple assertions testing

--- lib/testing.sh
+++ lib/testing.sh
@@ -4,3 +4,6 @@ RUN "ansible-playbook assert.yml --tags crypto"
 ECHO "Asserção para testar se a quantidade de memória é suficiente para instalar o Blender."
 RUN "ansible-playbook assert.yml --tags blender"
 
+ECHO "Asserções para testar vários requisitos."
+RUN "ansible-playbook assert.yml --tags reqts"
+
