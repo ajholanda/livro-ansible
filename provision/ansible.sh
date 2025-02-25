6f7946caa2239ea32defcadb17d071ae199bb87e
Adriano J. Holanda <aholanda@usp.br>
2025-02-25 17:17:09 -0300

    revamp to install only the essential packages

--- provision/ansible.sh
+++ provision/ansible.sh
@@ -1,22 +1,10 @@
 
-pkgs=(ansible pipx python3-pip sshpass)
+pkgs=(ansible ansible-lint python3-pip sshpass yamllint)
 for pkg in "${pkgs[@]}"
 do
     echo "APT: Installing ${pkg}..."
     DEBIAN_FRONTEND=noninteractive apt-get install -y ${pkg} >/dev/null
 done
-
-pkgs=(ansible-lint molecule yamllint)
-for pkg in "${pkgs[@]}"
-do
-	echo "PIPx: Installing ${pkg}..."
-	sudo -u vagrant pipx install ${pkg} >/dev/null
-done
-
-# Add programs installed by pipx in the PATH.
-sudo -u vagrant pipx ensurepath
-
-sudo -u vagrant pip install molecule-plugins[docker]
 
 echo "DONE"
 
