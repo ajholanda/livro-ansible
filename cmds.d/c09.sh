f62871c817c7a318566dd8128aecec804ba5c129
Adriano J. Holanda <aholanda@usp.br>
2025-06-12 14:59:48 -0300

    Aninhamento dos comandos dentro da função c09

--- cmds.d/c09.sh
+++ cmds.d/c09.sh
@@ -1,5 +1,17 @@
-# Ansible Galaxy
-function galaxy() {
+function c09() {
+	echo "Capítulo 9 - Ansible Galaxy"
+	echo "========================================="
+
+	# Ansible Galaxy
+	ECHO "Faz download do role ajholanda.x2goclient"
+	RUN "ansible-galaxy role install --roles-path ./roles ajholanda.x2goclient"
+
+	ECHO "Instala o cliente X2Go nos hosts pertencentes ao grupo de TI (infotech) "
+	RUN "ansible-playbook desktops.yml --tags x2goclient --limit infotech"
+
+	ECHO "Remove o cliente X2Go nos hosts pertencentes ao grupo de TI (infotech) "
+	RUN "ansible-playbook desktops.yml --tags x2goclient --limit infotech --extra-vars \"package_state=absent\""
+
 	ECHO "Instala os módulos para o Windows"
 	RUN "ansible-galaxy collection install ansible.windows"
 	RUN "ansible-galaxy collection install chocolatey.chocolatey"
@@ -8,4 +20,3 @@ function galaxy() {
 	RUN "ansible-galaxy role install --force --roles-path ./roles ajholanda.googlechrome"
 	RUN "ansible-galaxy role install --force --roles-path ./roles ajholanda.vscode"
 }
-
