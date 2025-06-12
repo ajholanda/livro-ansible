5568d96e1950b51472438cda3b8a8e8a5ed15cc8
Adriano J. Holanda <aholanda@usp.br>
2025-06-12 14:58:42 -0300

    Aninhamento dos comandos dentro da função c05

--- cmds.d/c05.sh
+++ cmds.d/c05.sh
@@ -1,5 +1,7 @@
-# Ansible roles
-function exec_roles_cmds() {
+function c05() {
+	echo "Capítulo 5 - Ansible roles"
+	echo "========================================="
+
 	ECHO "Executa o playbook webserver-role.yml (role webserver_debian) somente no host web.example.net"
 	RUN 'ansible-playbook webserver-role.yml --tags webserver_debian'
 }
