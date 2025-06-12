c7568113729d2bdb835fc95257ff31fee8f73761
Adriano J. Holanda <aholanda@usp.br>
2025-06-12 14:54:25 -0300

    Alteração do tratamento dos argumentos e do menu de ajuda

--- cmds.sh
+++ cmds.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-DEBUG=0
-
-# Load functions from chapters.
-for f in {01..12}; do
-	. /lib/c${f}.sh
-done
+# Base directory where the script is located.
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Debug forces a pause after each command.
+DEBUG=1
+# Dry run only print the commands on the screen.
+DRY_RUN=0
 
 # Helper function to print on stderr.
 function ECHO {
@@ -15,84 +15,99 @@ function ECHO {
 # Helper function to execute commands.
 function RUN {
 	echo $1
+
 	if [ $DEBUG -eq 1 ]; then
 		read -p 'Press any key to continue: ' tmp
 	fi
-	eval $1 && test 1 -eq 1
-	sleep 3
+	if [ $DRY_RUN -eq 1 ]; then
+		echo "DRY> no operation executed ooo"
+	else
+		eval $1 && test 1 -eq 1
+	fi
+	sleep 2
 }
 
+function setup_galaxy {
+	echo "Instala coleções do Ansible Galaxy"
+	echo "========================================="
+	ansible-galaxy collection install ansible.windows
+	ansible-galaxy collection install chocolatey.chocolatey
+	ansible-galaxy collection install community.general
+	ansible-galaxy collection install community.windows
+}
+
+function usage {
 USAGE=$(
-	cat <<-EOM
-		$0 [--all | -c{02..12]} ] [--debug]
-		onde cada opção executa os comandos:
-		  all       todos
-		  c02       do Capítulo 2 - comandos ad-hoc
-		  c03       do Capítulo 3 - playbook
-		  c04       do Capítulo 4 - variáveis
-		  c05       do Capítulo 5 - roles
-		  c06       do Capítulo 6 - gabarito
-		  c07       do Capítulo 7 - criptografia
-		  c08       do Capítulo 8 - desenvolvimento de plugins e modulos
-		  c09       do Capítulo 9 - Ansible Galaxy
-		  c10       do Capítulo 10 - testes
-		  c11       do Capítulo 11 - Windows
-		  c12       do Capítulo 12 - casos de uso
-		--debug: pausa a execução a cada comando.
+		cat <<-EOM
+		$0 [c{00..12]}] [--dry-run | --no-debug | --help]
+		  onde:
+		    c00       	Executa os comandos de todos os capítulos.
+		    c01	    	Capítulo 1 - Automação e Ansible (sem comandos)
+		    c02       	Capítulo 2 - Comandos ad-hoc
+		    c03       	Capítulo 3 - Playbook
+		    c04       	Capítulo 4 - Variáveis
+		    c05       	Capítulo 5 - Ansible roles
+		    c06       	Capítulo 6 - Gabarito
+		    c07       	Capítulo 7 - Criptografia
+		    c08       	Capítulo 8 - Desenvolvimento de plugins e modulos
+		    c09       	Capítulo 9 - Ansible Galaxy
+		    c10       	Capítulo 9 - Depuração
+		    c11       	Capítulo 11 - Testes
+		    c12       	Capítulo 12 - Windows
+		    c13       	Capítulo 13 - Casos de uso
+		    --no-debug:	Executa os comandos sem pausas.
+		    --dry-run: 	Lista os comandos sem executá-los.
+		    --help:	Imprime esta mensagem.
+
+		Exemplos:
+		$0 c02 c04 --no-debug --dry-run
+		  Lista os comandos dos capítulos 2 e 4 sem pausas.
+		$0 c03 c04 c05
+		  Executa os comandos dos capítulos 3, 4 e 5 com pausa entre as execuções.
+		$0 c00
+		  Executa todos os comandos com pausa entre as execuções.
 	EOM
 )
+	echo >&2 "$USAGE"
+}
 
-if [ $2 == "--debug" ]; then
-	DEBUG=1
+if [ $# -eq 0 ]; then
+	usage
+	exit 1
 fi
 
-case $1 in
-"c02")
-	exec_adhoc_cmds
-	;;
-"c03")
-	exec_playbook_cmds
-	;;
-"c04")
-	exec_vars_cmds
-	;;
-"c05")
-	exec_roles_cmds
-	;;
-"c06")
-	exec_template_cmds
-	;;
-"c07")
-	exec_crypto_cmds
-	;;
-"c08")
-	exec_dev_cmds
-	;;
-"c09")
-	galaxy
-	;;
-"c10")
-	exec_tests
-	;;
-"c11")
-	exec_windows_cmds
-	;;
-"c12")
-	exec_usecases_cmds
-	;;
-"--galaxy")
-	galaxy
-	;;
-"--linux")
-	exec_linux_cmds
-	;;
-"--windows")
-	exec_windows_cmds
-	;;
-*)
-	echo >&2 "$USAGE"
+# Handle arguments.
+chapts=()
+for arg in "$@"; do
+	if [ -e "$BASEDIR/cmds.d/${arg}.sh" ]; then
+		chapts+=${arg}
+		shift
+	elif [ $arg == "--no-debug" ]; then
+		DEBUG=0
+		shift
+	elif [ $arg == "--dry-run" ]; then
+		DRY_RUN=1
+		shift
+	elif [ $arg == "--help" ]; then
+		usage
+		exit 1
+	fi
+done
+
+# After all the shifts, the number of arguments must be zero.
+if [ $# -gt 0 ]; then
+	usage
 	exit 1
-	;;
-esac
+fi
+
+# Load all the functions that execute commands from book chapters.
+for file in `ls -r $BASEDIR/cmds.d`; do
+       source $BASEDIR/cmds.d/${file}
+done
+
+# Execute commands from each chapter passed as argument.
+for chapt in "${chapts[@]}"; do
+	eval $chapt
+done
 
 exit 0
