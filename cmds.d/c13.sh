03b7d5a522a18476be304f67f996ce61d44c0c0b
Adriano J. Holanda <aholanda@usp.br>
2025-06-12 15:03:18 -0300

    Migração dos comandos para os casos de uso para cá

---
+++ cmds.d/c13.sh
@@ -0,0 +1,35 @@
+function c13() {
+		echo "Capítulo 13 - Casos de uso"
+		ECHO "========================================="
+
+	setup_galaxy
+
+	ECHO "Configura servidor(es)"
+	RUN "ansible-playbook servers.yml"
+
+	ECHO "Gerenciamento de computadores (desktops)"
+	ECHO "Instala os programas nos hosts do grupo desktops"
+	RUN "ansible-playbook desktops.yml"
+	ECHO "Instala os programas nos hosts do grupo labs"
+	RUN "ansible-playbook desktops.yml --limit lab"
+	ECHO "Instala o VS Code no host alice"
+	RUN "ansible-playbook desktops.yml --limit inf1.example.net --tags vscode"
+	ECHO "Remove o Google Chrome do host pedro"
+	RUN "ansible-playbook desktops.yml --limit off1.example.net --tags googlechrome --extra-vars 'package_state=absent'"
+	ECHO "Atualiza todos os programas do grupo desktops"
+	RUN "ansible-playbook desktops.yml --extra-vars 'package_state=latest'"
+
+	ECHO "Força uma atualização no Windows usando o Windows Update (com reboots)"
+	RUN "ansible-playbook desktops.yml --tags win_updates"
+	ECHO "Atualiza os programas que foram instalados no Windows usando o módulo win_chocolatey do Ansible."
+	RUN "ansible-playbook desktops.yml --tags win_chocolatey --limit windows -e 'package_state=latest'"
+
+	# CONTAINER (Docker)
+	ECHO "Instala o Docker nos hosts do grupo cloud"
+	RUN "ansible-playbook docker.yml --tags docker"
+	ECHO "Instala o PHP+Apache no contêiner dos hosts do grupo cloud"
+	RUN "ansible-playbook docker.yml --tags docker_php_apache"
+	ECHO "Instala o PostgreSQL no contêiner dos hosts do grupo cloud"
+	RUN "ansible-playbook docker.yml --tags docker_postgres"
+}
+
