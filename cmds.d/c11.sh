ac317a14d1c08609da65021912618f10c4b812a6
Adriano J. Holanda <aholanda@usp.br>
2025-06-12 15:00:12 -0300

    Aninhamento dos comandos dentro da função c11

--- cmds.d/c11.sh
+++ cmds.d/c11.sh
@@ -1,18 +1,40 @@
-# Windows
-function exec_windows_cmds() {
-	galaxy
+function c11 {
+    echo "Capítulo 11 - Testes"
+    echo "========================================="
 
-	# win_chocolatey module
-	ECHO "Instala o LibreOffice no sistem Windows"
-	RUN "ansible-playbook desktops.yml --tags libreoffice --limit windows"
+    ECHO "Asserção para assegurar o uso de criptografia nas senhas"
+    RUN "ansible-playbook assert.yml --tags crypto"
 
-	# win_powershell module
-	ECHO "Executa um script PowerShell para agendar desligamento"
-	RUN "ansible-playbook desktops.yml --tags power --limit windows"
+    ECHO "Asserção para testar se a quantidade de memória é suficiente para instalar o Blender"
+    RUN "ansible-playbook assert.yml --tags blender"
 
-	# win_user module
-	ECHO "Gerencia usuários no Windows"
-	RUN "ansible-playbook  desktops.yml --tags user --limit windows"
+    ECHO "Asserções para testar vários requisitos."
+    RUN "ansible-playbook assert.yml --tags reqts"
+
+    ECHO "Instala o Molecule, suas dependências, o driver docker e o Docker"
+    RUN "ansible-playbook molecule.yml"
+
+    ECHO "Configura um ambiente (Docker) de testes Molecule padrão"
+    RUN "molecule init scenario --driver-name docker"
+
+    ECHO "Cria o cenário office usando o driver padrão"
+    RUN "molecule init scenario office"
+
+    ECHO "Cria o ambiente para o cenário padrão"
+    RUN "molecule create"
+
+    ECHO "Executa a preparação (prepare.yml)"
+    RUN "molecule prepare"
+
+    ECHO "Executa o teste (converge.yml)"
+    RUN "molecule converge"
+
+    ECHO "Executa o verificador (verifier)"
+    RUN "molecule verify"
+
+    ECHO "Realiza o ciclo de vida completo de teste"
+    RUN "molecule test"
+
+    ECHO "Realiza o teste no cenário office"
+    RUN  "molecule test -s office"
 }
-
-
