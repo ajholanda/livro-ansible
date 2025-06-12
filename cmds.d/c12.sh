e5176490f9e405a7a6daa455bc8928f00f2007b5
Adriano J. Holanda <aholanda@usp.br>
2025-06-12 15:00:31 -0300

    Aninhamento dos comandos dentro da função c12

--- cmds.d/c12.sh
+++ cmds.d/c12.sh
@@ -1,33 +1,20 @@
-# Casos de uso
-function exec_usecases_cmds() {
-	galaxy
+function c12() {
+	echo "Capítulo 12 - Windows"
+	echo "========================================="
 
-	ECHO "Configura servidor(es)"
-	RUN "ansible-playbook servers.yml"
+	setup_galaxy
 
-	ECHO "Gerenciamento de computadores (desktops)"
-	ECHO "Instala os programas nos hosts do grupo desktops"
-	RUN "ansible-playbook desktops.yml"
-	ECHO "Instala os programas nos hosts do grupo labs"
-	RUN "ansible-playbook desktops.yml --limit lab"
-	ECHO "Instala o VS Code no host alice"
-	RUN "ansible-playbook desktops.yml --limit inf1.example.net --tags vscode"
-	ECHO "Remove o Google Chrome do host pedro"
-	RUN "ansible-playbook desktops.yml --limit off1.example.net --tags googlechrome --extra-vars 'package_state=absent'"
-	ECHO "Atualiza todos os programas do grupo desktops"
-	RUN "ansible-playbook desktops.yml --extra-vars 'package_state=latest'"
+	# win_chocolatey module
+	ECHO "Instala o LibreOffice no sistem Windows"
+	RUN "ansible-playbook desktops.yml --tags libreoffice --limit windows"
 
-	ECHO "Força uma atualização no Windows usando o Windows Update (com reboots)"
-	RUN "ansible-playbook desktops.yml --tags win_updates"
-	ECHO "Atualiza os programas que foram instalados no Windows usando o módulo win_chocolatey do Ansible."
-	RUN "ansible-playbook desktops.yml --tags win_chocolatey --limit windows -e 'package_state=latest'"
+	# win_powershell module
+	ECHO "Executa um script PowerShell para agendar desligamento"
+	RUN "ansible-playbook desktops.yml --tags power --limit windows"
 
-	# CONTAINER (Docker)
-	ECHO "Instala o Docker nos hosts do grupo cloud"
-	RUN "ansible-playbook docker.yml --tags docker"
-	ECHO "Instala o PHP+Apache no contêiner dos hosts do grupo cloud"
-	RUN "ansible-playbook docker.yml --tags docker_php_apache"
-	ECHO "Instala o PostgreSQL no contêiner dos hosts do grupo cloud"
-	RUN "ansible-playbook docker.yml --tags docker_postgres"
+	# win_user module
+	ECHO "Gerencia usuários no Windows"
+	RUN "ansible-playbook  desktops.yml --tags user --limit windows"
 }
+
 
