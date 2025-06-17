39c49d04de9e9fc38d97287d7e6125dac44d058d
Adriano J. Holanda <aholanda@usp.br>
2025-06-17 11:22:01 -0300

    Adição do comando para executar o role webserver #1

--- cmds.d/c05.sh
+++ cmds.d/c05.sh
@@ -4,4 +4,7 @@ function c05() {
 
 	ECHO "Executa o playbook webserver-role.yml (role webserver_debian) somente no host web.example.net"
 	RUN 'ansible-playbook webserver-role.yml --tags webserver_debian'
+
+	ECHO "Executa o playbook webserver-role.yml nos hosts do grupo webservers"
+	RUN 'ansible-playbook webserver-role.yml'
 }
