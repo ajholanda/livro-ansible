e8dfdfb2be72727527035e64047faa74b7cbdc5d
Adriano J. Holanda <aholanda@usp.br>
2025-06-20 17:58:37 -0300

    Retirada de nova linha

--- cmds.d/c07.sh
+++ cmds.d/c07.sh
@@ -6,10 +6,10 @@ function c07() {
 	ECHO "Executa o role rsyncserver usando o --vault-id"
 	RUN "ansible-playbook servers.yml --vault-id vault.txt --limit rsyncservers --tags rsyncserver"
 
-	ECHO "Criptografa um arquivo usando o vault ID prod"
-	RUN "ansible-vault encrypt --vault-id prod@vault-prod.txt sssd/templates/sssd.conf.j2 --encrypt-vault-id prod 2>/dev/null"
-
 	## Criptografia de variáveis
 	ECHO "Ajusta senha para um usuário do MariaDB a partir de um vault"
 	RUN 'ansible-playbook servers.yml --limit dbservers --tags mariadbserver'
+	
+	ECHO "Criptografa um arquivo usando o vault ID prod"
+	RUN "ansible-vault encrypt --vault-id prod@vault-prod.txt sssd/templates/sssd.conf.j2 --encrypt-vault-id prod 2>/dev/null"
 }
