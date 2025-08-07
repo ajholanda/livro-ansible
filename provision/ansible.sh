bfc69ec9463b6f3bd9735585fff520da9b1f3ebd
Adriano J. Holanda <aholanda@usp.br>
2025-08-07 17:07:33 -0300

    Instalação do ansible e yamllint via apt-get.

--- provision/ansible.sh
+++ provision/ansible.sh
@@ -1,9 +1,17 @@
+# Define system packages and Python packages.
+apt_pkgs="ansible make python3-pip sshpass tree virtualenv yamllint"
+py_pkgs="ansible-lint"
 
-apt_pkgs="make python3-pip sshpass tree virtualenv"
+# ---
+# Section 1: Install system packages.
+# ---
 echo "APT: Installing ${apt_pkgs}..."
-DEBIAN_FRONTEND=noninteractive apt-get install -y ${apt_pkgs} >/dev/null
+DEBIAN_FRONTEND=noninteractive apt-get update >/dev/null && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y ${apt_pkgs} >/dev/null
 
-py_pkgs="ansible ansible-lint yamllint"
+# ---
+# Section 2: Install Python packages.
+# ---
 echo "PIP: Installing ${py_pkgs}..."
 pip install $py_pkgs
 
