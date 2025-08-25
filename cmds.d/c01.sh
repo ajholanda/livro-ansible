0456642979affc7dcc296fc6a7a07968b84531bb
Adriano J. Holanda <aholanda@usp.br>
2025-08-25 15:45:25 -0300

    Adição de comandos de listagem do inventário dinâmico

--- cmds.d/c01.sh
+++ cmds.d/c01.sh
@@ -2,4 +2,15 @@ function c01 {
 	echo "Capítulo 1. Automação e Ansible"
 	echo "========================================="
 	echo "Seja bem-vindo(a) ao curso de Ansible!"
+
+	make /etc/ansible/inventory.py
+
+	cd /tmp
+	ECHO "Lista os hosts de um inventário dinâmico"
+	RUN "ansible-inventory -i /etc/ansible/inventory.py --list"
+
+	ECHO "Lista os hosts de um inventário dinâmico de forma hieráquica"
+	RUN "ansible-inventory -i /etc/ansible/inventory.py --graph"
+
+	cd $HOME/livro
 }
