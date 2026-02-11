8e6b71cf5d4c913c5d7cfeedf1feb0bd2de15df4
Adriano J. Holanda <ajholanda@gmail.com>
2026-02-11 15:49:15 -0300

    Conversão da lista de usuários (user_admins) em dicionário para adicionar passwd

---
+++ scripts/pwd-hashgen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Generate a hash using SHA512 algorithm.
+
+if [ $# -ne 1 ]; then
+	echo "Error!"
+	echo "Usage: $0 <password>"
+	exit 1
+fi
+
+ansible all -i localhost, -m debug -a "msg={{ '$1' | password_hash('sha512') }}"
+
+exit 0
