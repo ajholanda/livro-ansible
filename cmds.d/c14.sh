cd188faadccf269a26e01397f3c9632807f47a48
Adriano J. Holanda <ajholanda@gmail.com>
2026-05-07 17:33:51 -0300

    Adição de lembrete sobre a instalação do AWX

---
+++ cmds.d/c14.sh
@@ -0,0 +1,5 @@
+function c12() {
+	ECHO_CHAPTER "14" "AWX"
+
+	echo "Antes de executar o comando `ansible-playbook awx-setup.yml`, leia as instruções de pré-requisitos e ajuste de recursos no playbook awx-setup.yml."
+}
