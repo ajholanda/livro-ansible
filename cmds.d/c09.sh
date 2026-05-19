7a64513694900d169610e7d5fe268522e79c234d
Adriano J. Holanda <ajholanda@gmail.com>
2026-05-19 17:23:34 -0300

    Adição de comandos para geração e execução de um EE

--- cmds.d/c09.sh
+++ cmds.d/c09.sh
@@ -20,4 +20,21 @@ function c09() {
 
 	ECHO "Instala as coleções e roles usando requirements.yml"
 	RUN "ansible-galaxy install -r requirements.yml --force"
+
+	# Construção de EEs
+	echo "No comando a seguir há um problema de permissões"
+	echo "com o diretório livro/, sincronizado pelo Vagrant:"
+	echo "$ ansible-builder build -t my-ee:1.0"
+	echo ""
+	echo "Vamos contornar o problema usando o diretório /tmp"
+	echo "para gerar a imagem, conforme mostrado nos comandos a seguir:"
+	echo "$ ansible-builder create --context /tmp/context"
+	echo "$ docker build -f /tmp/context/Dockerfile -t my-ee:1.0 /tmp/context"
+	echo ""
+	ECHO "Cria a image do Execution Environment definido em execution-environment.yml"
+	RUN "ansible-builder create --context /tmp/context"
+	RUN "docker build -f /tmp/context/Dockerfile -t my-ee:1.0 /tmp/context"
+
+	ECHO "Executa o playbook utilizando o EE gerado"
+	RUN "ansible-navigator run site.yml"
 }
