793cbfde31fcd366ebb291e305afe6a6a5bfb112
Adriano J. Holanda <ajholanda@gmail.com>
2026-06-11 12:16:52 -0300

    Os comandos foram transferidos para comandos/README.md

--- cmds.d/c00.sh
+++
@@ -1,9 +0,0 @@
-function c00 {
-	echo "=========================================="
-	echo "* Executando todos os comandos."
-	echo "=========================================="
-	curr_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-	for path in `ls ${curr_dir}/*.sh | grep -v c00`; do
-       		$(basename $path .sh)
-	done
-}
