18afd92088ee93c1a5e2d28ad54915d6a4ce01ff
Adriano J. Holanda <ajholanda@gmail.com>
2026-07-16 15:34:27 -0300

    Remoção após recuperar alguns comandos

--- cmds.d/c05.sh
+++
@@ -1,9 +0,0 @@
-function c05() {
-	ECHO_CHAPTER "5" "Ansible roles"
-
-	ECHO "Executa o playbook webserver-role.yml (role webserver_debian) somente no host web.example.net"
-	RUN 'ansible-playbook webserver-role.yml --tags webserver_debian'
-
-	ECHO "Executa o playbook webserver-role.yml nos hosts do grupo webservers"
-	RUN 'ansible-playbook webserver-role.yml'
-}
